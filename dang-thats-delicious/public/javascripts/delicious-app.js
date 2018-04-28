8b411db7aaf6794d6d9975de9b2a07267fc58a50
David Chapman <davidchappy@gmail.com>
2018-04-28 15:25:52 -0500

    Complete 16

--- dang-thats-delicious/public/javascripts/delicious-app.js
+++ dang-thats-delicious/public/javascripts/delicious-app.js
@@ -1,3 +1,8 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  autocomplete( $('#address'), $('#lat'), $('#lng') );
+});
---
+++ dang-thats-delicious/public/javascripts/modules/autocomplete.js
@@ -0,0 +1,18 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return;
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    const { lat, lng } = place.geometry.location;
+    latInput.value = Math.round(lat());
+    lngInput.value = Math.round(lng());
+  })
+
+  // If someone hits enter on the address field, don't submit the form
+  input.on('keydown', e => {
+    if (e.keyCode === 13) e.preventDefault();
+  });
+}
+
+export default autocomplete;

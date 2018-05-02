c410411acf841e3e9af870353851b2902125a2c0
David Chapman <davidchappy@gmail.com>
2018-05-02 14:46:31 -0500

    Complete 23

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -26,5 +27,13 @@ router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate registration data
+// 2. Register the user
+// 3. Log in the user
+router.post('/register', userController.validateRegister);
 
 module.exports = router;

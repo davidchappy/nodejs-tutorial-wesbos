f8208ad1e7bb4264c336549c8b4c6fc3e9d4b8c8
David Chapman <davidchappy@gmail.com>
2018-05-03 13:14:56 -0500

    Complete 28

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 const User = mongoose.model('User');
 
@@ -43,7 +44,13 @@ exports.forgot = async (req, res) => {
 
   // 3. Send an email with the token
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `You have been emailed a password reset. ${resetURL}`);
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset'
+  });
+  req.flash('success', `You have been emailed a password reset.`);
 
   // 4. Redirect to login page
   res.redirect('/login');

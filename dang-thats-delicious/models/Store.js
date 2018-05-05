ad2a5c1558900d4ffba5884f64fadeb4d53c9498
David Chapman <davidchappy@gmail.com>
2018-05-05 09:49:45 -0500

    Complete 38

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
     ref: 'User',
     required: 'You must supply an author'
   }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
 
 // Define our indexes
@@ -74,5 +77,11 @@ storeSchema.statics.getTagsList = function() {
     { $sort: { count: -1 }}
   ]);
 }
+
+storeSchema.virtual('reviews', {
+  ref: 'Review', // what model to link
+  localField: '_id', // which field on the store
+  foreignField: 'store' // which field on the review
+});
 
 module.exports = mongoose.model('Store', storeSchema);
